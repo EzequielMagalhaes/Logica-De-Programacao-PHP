9c4900c6f9edc28e26118777d30b341232bdef09
Ezequiel Magalhaes <zekes_243@hotmail.com>
2022-12-28 11:21:55 -0300

    Salvando curso de logica PHP

--- aula05.php
+++ aula05.php
@@ -1,5 +1,21 @@
 <?php
+    // Preencher array sem números repetidos;
+    $arr = [];
+    for ($i = 0; $i < 5; $i++){
+        $arr[$i] = rand(1,5); //gera os numeros aleatórios; quanto maior o seundo numero, menor as chances de cair um numero repetido;
+        while(inArrayCustom($i,$arr[$i],$arr)){
+            $arr[$i] = rand (1,5);
+        }
+    }
 
-
-
+    print_r($arr);
+    function inArrayCustom($indice,$valor,$arr){
+        for($i = 0; $i < count($arr); $i++){
+            if($arr[$i] == $valor && $i != $indice){
+                echo 'Repetiu!<br>'; //Toda vez que ele precisar retornar ao loop para nao gerar numeros repetidos, ele vai exibir a mensagem de 'Repetiu!'
+                return true;
+            }
+        }
+        return false;
+    }
 ?>
